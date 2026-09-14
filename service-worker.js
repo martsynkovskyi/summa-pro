f0ed9105616bb7b960154f093eeced0ab90d27b1
Marik Ander <martsynkovskyi@MacBook-Air-von-Marik.local>
2026-09-14 15:24:33 +0200

    Align result text across fields

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const CACHE_PREFIX = "summa-propisyu-";
-const CACHE_NAME = "summa-propisyu-v3.0-20260914-release15";
+const CACHE_NAME = "summa-propisyu-v3.0-20260914-release16";
 const OFFLINE_URL = "./index.html";
 const APP_SHELL = [
   "./index.html",
